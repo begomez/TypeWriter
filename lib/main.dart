b3ee3dab8669b3cb79431c88e3c28326372022ad
Bernat Gómez <bernat.gomez@gmail.com>
2021-06-08 22:52:28 +0200

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -5,72 +5,90 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TyperText',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'TyperText'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(this.title),
       ),
       body: Container(
-        height: 100,
-        width: double.maxFinite,
-        color: Colors.yellow,
         alignment: Alignment.center,
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: StreamBuilder<String>(
-          stream: this.getData(),
-          initialData: "",
-          builder: (BuildContext cntxt, AsyncSnapshot<String> inc) {
-            if (inc.hasData) {
-              //print(this.msg.toString());
-
-              return Text(
-                inc.data,
-                //key: ObjectKey(this.msg.toString().hashCode),
-                style: TextStyle(fontSize: 18, color: Colors.red),
-              );
-            } else {
-              return Text("");
-            }
-          },
+        color: Colors.white,
+        child: TyperTextWidget(
+          data: StreamableTextModel(
+              msg: "The quick brown fox jumps over the lazy dog"),
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
 
-  Stream<String> getData() async* {
-    String msg = "You have pushed the button this many times....";
+/**
+ * Widget that displays text using a typewriter effect
+ */
+class TyperTextWidget extends StatelessWidget {
+  final StreamableTextModel data;
 
+  const TyperTextWidget({@required this.data, Key key})
+      : assert(data != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<String>(
+        initialData: "",
+        stream: this.data.toStream(),
+        builder: (BuildContext cntxt, AsyncSnapshot<String> snap) {
+          if (snap.hasData) {
+            return Text(snap.data);
+          } else {
+            return Text(
+              "",
+              maxLines: 3,
+            );
+          }
+        });
+  }
+}
+
+/**
+ * Helper data model
+ */
+class StreamableTextModel {
+  static const INITIAL_DELAY = 1000;
+  static const STEP_DELAY = 50;
+  final String msg;
+
+  const StreamableTextModel({this.msg = ""}) : assert(msg != null);
+
+  factory StreamableTextModel.empty() => StreamableTextModel(msg: "");
+
+  int _getDelayForIteration(int iteration) =>
+      iteration == 0 ? INITIAL_DELAY : STEP_DELAY;
+
+  Stream<String> toStream() async* {
     for (var i = 0; i < msg.length; i++) {
-      await Future.delayed(Duration(milliseconds: i == 0 ? 1000 : 50));
-      yield msg.substring(0, i);
+      await Future.delayed(
+          Duration(milliseconds: this._getDelayForIteration(i)));
+
+      yield msg.substring(0, i + 1);
     }
   }
 }
